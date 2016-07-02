6f2cfcfcff9e3f1444c7b31b3e0a116d9be8ac2c
wvanheemstra <willem@vanheemstrasystems.com>
2016-07-02 17:30:09 +0100

    Modified

--- DBACh04/dist/js/views/todos.js
+++ DBACh04/dist/js/views/todos.js
@@ -16,7 +16,9 @@ app.TodoView = Backbone.View.extend({
 
   // The DOM events specific to an item.
   events: {
+    'click .toggle': 'toggleCompleted',
     'dblclick label': 'edit',
+    'click .destroy': 'clear',
     'keypress .edit': 'updateOnEnter',
     'blur .edit': 'close'
   },
@@ -25,32 +27,61 @@ app.TodoView = Backbone.View.extend({
   // a one-to-one correspondence between a **Todo** and a **TodoView** in this
   // app, we set a direct reference on the model for convenience.
   initialize: function() {
-	console.log('views/todos.js - inside app.TodoView initialize()');	  
+	console.log('views/todos.js - inside app.TodoView initialize()');
     this.listenTo(this.model, 'change', this.render);
+    this.listenTo(this.model, 'destroy', this.remove);
+    this.listenTo(this.model, 'visible', this.toggleVisible);
   },
 
-  // Re-renders the titles of the todo item.
+  // Re-render the titles of the todo item.
   render: function() {
-	console.log('views/todos.js - inside app.TodoView render()');	  
+	console.log('views/todos.js - inside app.TodoView render()');
     this.$el.html( this.template( this.model.attributes ) );
+
+    this.$el.toggleClass( 'completed', this.model.get('completed') );
+    this.toggleVisible();
+
     this.$input = this.$('.edit');
     return this;
   },
 
+  // Toggles visibility of item
+  toggleVisible : function () {
+	console.log('views/todos.js - inside app.TodoView toggleVisible()');
+    this.$el.toggleClass( 'hidden',  this.isHidden());
+  },
+
+  // Determines if item should be hidden
+  isHidden : function () {
+	console.log('views/todos.js - inside app.TodoView isHidden()');
+    var isCompleted = this.model.get('completed');
+    return ( // hidden cases only
+      (!isCompleted && app.TodoFilter === 'completed') || (isCompleted && app.TodoFilter === 'active')
+    );
+  },
+
+  // Toggle the `"completed"` state of the model.
+  toggleCompleted: function() {
+	console.log('views/todos.js - inside app.TodoView toggleCompleted()');
+    this.model.toggle();
+  },
+
   // Switch this view into `"editing"` mode, displaying the input field.
   edit: function() {
-	console.log('views/todos.js - inside app.TodoView edit()');	  
+	console.log('views/todos.js - inside app.TodoView edit()');
     this.$el.addClass('editing');
     this.$input.focus();
   },
 
   // Close the `"editing"` mode, saving changes to the todo.
   close: function() {
-	console.log('views/todos.js - inside app.TodoView close()');		  
+	console.log('views/todos.js - inside app.TodoView close()');
     var value = this.$input.val().trim();
 
     if ( value ) {
       this.model.save({ title: value });
+    } else {
+      this.clear();
     }
 
     this.$el.removeClass('editing');
@@ -58,9 +89,15 @@ app.TodoView = Backbone.View.extend({
 
   // If you hit `enter`, we're through editing the item.
   updateOnEnter: function( e ) {
-	console.log('views/todos.js - inside app.TodoView updateOnEnter()');	
+  console.log('views/todos.js - inside app.TodoView updateOnEnter(e)');
     if ( e.which === ENTER_KEY ) {
       this.close();
     }
+  },
+
+  // Remove the item, destroy the model from *localStorage* and delete its view.
+  clear: function() {
+    console.log('views/todos.js - inside app.TodoView clear()');
+    this.model.destroy();
   }
 });
