d10b9a4804ed44666b18fa1a0734e511bf02ea90
wvanheemstra <willem@vanheemstrasystems.com>
2016-07-02 17:51:33 +0100

    Modified

--- DBACh04/dist/js/dbach04.js
+++ DBACh04/dist/js/dbach04.js
@@ -82,7 +82,34 @@ toggle: function() {
   });
 }
 
-});;;// js/views/app.js
+});;// js/routers/router.js
+
+// Todo Router
+// ----------
+
+var app = app || {};
+
+var Workspace = Backbone.Router.extend({
+  routes:{
+    '*filter': 'setFilter'
+  },
+
+  setFilter: function( param ) {
+	console.log('routers/router.js - inside app.ToDoRouter setFilter(param)');
+    // Set the current filter to be used
+    if (param) {
+      param = param.trim();
+    }
+    app.TodoFilter = param || '';
+
+    // Trigger a collection filter event, causing hiding/unhiding
+    // of Todo view items
+    app.Todos.trigger('filter');
+  }
+});
+
+app.TodoRouter = new Workspace();
+Backbone.history.start();;// js/views/app.js
 
 var app = app || {};
 
