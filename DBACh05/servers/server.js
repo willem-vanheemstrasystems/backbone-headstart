40be6e389a9586b5064792851c4afaff55386c49
wvanheemstra <willem@vanheemstrasystems.com>
2016-07-03 11:31:38 +0100

    Modified

--- DBACh05/servers/server.js
+++ DBACh05/servers/server.js
@@ -2,22 +2,110 @@
 var application_root = __dirname,
     express = require( 'express' ), //Web framework
     bodyParser = require('body-parser'), //Parser for reading request body
-    path = require( 'path' ); //Utilities for dealing with file paths
-    //mongoose = require( 'mongoose' ); //MongoDB integration
+    methodOverride = require('method-override'),
+    errorHandler = require('errorhandler'),
+    path = require( 'path' ), //Utilities for dealing with file paths
+    mongoose = require( 'mongoose' ); //MongoDB integration
+
+//Connect to database
+mongoose.connect( 'mongodb://localhost/library_database' );
+
+//Schemas
+var Book = new mongoose.Schema({
+    title: String,
+    author: String,
+    releaseDate: Date
+});
+
+//Models
+var BookModel = mongoose.model( 'Book', Book );
 
 //Create server
-var server = express();
+var Server = express();
 
-//Where to serve static content
-server.use( express.static( path.join( application_root,'../', 'dist') ) );
-server.use(bodyParser.urlencoded({
+/*
+* APP - The Application
+*/
+var App = express();
+/*
+* APP DEVELOPMENT
+*
+* .bash_profile contains
+* NODE_ENV=development
+*
+* or start server as follows
+* NODE_ENV=development node server.js
+*
+* on Windows use
+* set NODE_ENV=development
+* check with
+* echo %NODE_ENV%
+*/
+if('development' == App.settings.env) {
+  console.log("Using development configurations");
+  
+  // Parses request body and populates request.body
+  App.use(bodyParser.urlencoded({
   	extended: true
-})); // NEW IN CONNECT 3.0
-server.use(bodyParser.json()); // NEW IN CONNECT 3.0
+  })); // NEW IN CONNECT 3.0
+  App.use(bodyParser.json()); // NEW IN CONNECT 3.0
+
+  // Checks request.body for HTTP method overrides
+  App.use( methodOverride() );  
+
+  // Perform route lookup based on url and HTTP method
+  // App.use( App.router ); DEPRECATED !!
+  
+  // Where to serve static content
+  App.use( express.static( path.join( application_root,'../', 'dist') ) );
+
+  // Show all errors in development
+  App.use( errorHandler({ dumpExceptions: true, showStack: true }));  
+
+}
+
+/*
+* APP PRODUCTION
+*
+* .bash_profile contains
+* NODE_ENV=production
+*
+* or start server as follows
+* NODE_ENV=production node server.js
+*
+* on Windows use
+* set NODE_ENV=production
+* check with
+* echo %NODE_ENV%
+*/
+if('production' == App.settings.env){
+  console.log("Using production configurations");
+
+  // Parses request body and populates request.body
+  App.use(bodyParser.urlencoded({
+  	extended: true
+  })); // NEW IN CONNECT 3.0
+  App.use(bodyParser.json()); // NEW IN CONNECT 3.0
+
+  // Checks request.body for HTTP method overrides
+  App.use( methodOverride() );  
+
+  // Perform route lookup based on url and HTTP method
+  // App.use( App.router );  DEPRECATED !!
+  
+  // Where to serve static content
+  App.use( express.static( path.join( application_root,'../', 'dist') ) );
+  
+}
+
+//Routes
+App.get( '/api', function( request, response ) {
+    response.send( 'Library API is running' );
+});
 
 //Start server
-var port = 4712;
+var app_port = 4712;
 
-server.listen( port, function() {
-    console.log( 'Express server listening on port %d in %s mode', port, server.settings.env );
+Server.app_server = App.listen( app_port, function() {
+    console.log( 'Express server listening on port %d in %s mode', app_port, App.settings.env );
 });
